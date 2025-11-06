9316585ad7c59d9a4197d30eefea9757e4c22885
Umar <umar.envisionxperts@gmail.com>
2025-11-06 22:05:29 +0500

    Upwork icon add

--- team.php
+++ team.php
@@ -48,7 +48,8 @@ session_start();
                     'role' => 'Founder & CEO',
                     'img'  => 'team_img/muneeb_img_1.webp',
                     'bio'  => 'Meet Muneeb Sadiq – Founder & CEO of Envision Xperts, leading innovative digital solutions with a strong background in web and software development.',
-                    'social' => 'https://github.com/muneebsadiq'
+                    'github' => 'https://github.com/muneebsadiq',
+                    'upwork' => 'https://www.upwork.com/freelancers/~01c367b566d988f50f'
                 ],
                 [
                     'name' => 'Hafiz Syed Hanzala',
@@ -86,11 +87,25 @@ session_start();
                                 <h5 class="team-name mb-1"><?= htmlspecialchars($member['name']) ?></h5>
                                 <div class="team-role"><?= htmlspecialchars($member['role']) ?></div>
                                 <p class="team-bio mb-3"><?= htmlspecialchars($member['bio']) ?></p>
-                                <?php if (!empty($member['social'])): ?>
-                                    <a href="<?= htmlspecialchars($member['social']) ?>" target="_blank" rel="noopener noreferrer" class="btn btn-sm btn-outline-primary">
-                                        <i class="fab fa-<?= strpos($member['social'], 'github') !== false ? 'github' : 'linkedin' ?>"></i> 
-                                    </a>
-                                <?php endif; ?>
+                                <div class="team-social">
+                                    <?php if (!empty($member['github'])): ?>
+                                        <a href="<?= htmlspecialchars($member['github']) ?>" target="_blank" rel="noopener noreferrer" class="btn btn-sm btn-outline-primary me-2">
+                                            <i class="fab fa-github"></i>
+                                        </a>
+                                    <?php endif; ?>
+                                    <?php if (!empty($member['upwork'])): ?>
+                                        <a href="<?= htmlspecialchars($member['upwork']) ?>" target="_blank" rel="noopener noreferrer" class="btn btn-sm btn-outline-success" title="Upwork Profile">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 24 24">
+                                                <path d="M18.561 13.158c-1.102 0-2.135-.467-3.074-1.227l.228-1.076.008-.042c.207-1.143.849-3.06 2.839-3.06 1.492 0 2.703 1.212 2.703 2.703-.001 1.489-1.212 2.702-2.704 2.702zm0-8.14c-2.539 0-4.51 1.649-5.31 4.366-1.22-1.834-2.148-4.036-2.687-5.892H7.828v7.112c-.002 1.406-1.141 2.546-2.547 2.548-1.405-.002-2.543-1.143-2.545-2.548V3.492H0v7.112c0 2.914 2.37 5.303 5.281 5.303 2.913 0 5.283-2.389 5.283-5.303v-1.19c.529 1.107 1.182 2.229 1.974 3.221l-1.673 7.873h2.797l1.213-5.71c1.063.679 2.285 1.109 3.686 1.109 3 0 5.439-2.452 5.439-5.45 0-3-2.439-5.439-5.439-5.439z"/>
+                                            </svg>
+                                        </a>
+                                    <?php endif; ?>
+                                    <?php if (!empty($member['social'])): ?>
+                                        <a href="<?= htmlspecialchars($member['social']) ?>" target="_blank" rel="noopener noreferrer" class="btn btn-sm btn-outline-primary">
+                                            <i class="fab fa-<?= strpos($member['social'], 'github') !== false ? 'github' : 'linkedin' ?>"></i> 
+                                        </a>
+                                    <?php endif; ?>
+                                </div>
                             </div>
                         </div>
                     </div>
